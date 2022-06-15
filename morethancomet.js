06ba9b919125ce5cb463b75426859df54305ef7e
modda <support@moddapampe.de>
2022-06-15 10:48:40 +0200

    feat: typo fixed

--- morethancomet.js
+++ morethancomet.js
@@ -11,6 +11,6 @@ function draw() {
     background(0, 0, 0, 10);
     circle(brush0.x, brush0.y, 30);
 
-    brush.x += 1; // shorthand for 'brush0X = brush0X + 1'
+    brush0.x += 1; // shorthand for 'brush0X = brush0X + 1'
 }
 
