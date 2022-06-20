03549a10a6feff94241afeca2f221a6f59255692
modda <support@moddapampe.de>
2022-06-20 07:39:38 +0200

    final, really! :D

--- twin-brush.js
+++ twin-brush.js
@@ -10,19 +10,15 @@ function setup() {
       let green = 0;
       let opacity = 100;
   
-      // Change green and opacity 
-      // based on random diameter
       if (diameter < 20) {
         opacity = random(10, 40);
       } else {
         green = random(100, 200);
         opacity = random(40, 70);
       }
-  
-      // Define fill color
+
       fill(random(200, 255), green, 0, opacity);
-  
-      // Randomize x and y
+
       let x = mouseX + random(-20, 20);
       let y = mouseY + random(-20, 20);
       circle(x, y, diameter);
