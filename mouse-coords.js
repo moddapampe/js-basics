0074415fb01cea5ad554e8c1aeef9384f29a8909
modda <support@moddapampe.de>
2022-06-20 07:54:20 +0200

    initial

---
+++ mouse-coords.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+
+  
+  function draw() {
+    background(220);
+    if (mouseIsPressed) {
+
+      let coordinates = mouseX + "," + mouseY;
+      text(coordinates, mouseX, mouseY);
+    }
+  }
+  
