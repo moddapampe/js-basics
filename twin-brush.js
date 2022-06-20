d727d1b69a1c2bf7ce6198445bee6d441cb6c71d
modda <support@moddapampe.de>
2022-06-20 07:36:02 +0200

    final

--- twin-brush.js
+++ twin-brush.js
@@ -1,24 +1,31 @@
 function setup() {
     createCanvas(400, 400);
-}
-
-function draw() {
-    background('white');
+    background("white");
+    noStroke();
+  }
+  
+  function draw() {
     if (mouseIsPressed) {
-        let green = 0;
-        let diameter = random(25, 50);
-        let opacity = 100;
-
-        if (diameter > 25) {
-            opacity = random(10, 50)
-        } else {
-        green= random(100, 150);
-        opacity = random(50, 75);
+      let diameter = random(10, 30);
+      let green = 0;
+      let opacity = 100;
+  
+      // Change green and opacity 
+      // based on random diameter
+      if (diameter < 20) {
+        opacity = random(10, 40);
+      } else {
+        green = random(100, 200);
+        opacity = random(40, 70);
+      }
+  
+      // Define fill color
+      fill(random(200, 255), green, 0, opacity);
+  
+      // Randomize x and y
+      let x = mouseX + random(-20, 20);
+      let y = mouseY + random(-20, 20);
+      circle(x, y, diameter);
+      circle(x+10, y-10, diameter)
     }
-
-    fill(random(200, 255), green, 0, opacity);
-    let x = mouseX + random(-25, 25);
-    let y = mouseY + random(-25, 35);
-    circle(x, y, diameter)
-}
-}
+  }
