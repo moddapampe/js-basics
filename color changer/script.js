0dfafa88baec2b9fa7c707fd274b3176c6cfe721
modda <support@moddapampe.de>
2022-07-01 09:56:44 +0200

    final

--- color changer/script.js
+++ color changer/script.js
@@ -1,13 +1,13 @@
 
 function changeColor(){
-    let red = document.querySelector('sliderRed').value;
-    let green = document.querySelector('sliderGreen').value;
-    let blue = document.querySelector('sliderBlue').value;
+    let red = document.getElementById('sliderRed').value;
+    let green = document.getElementById('sliderGreen').value;
+    let blue = document.getElementById('sliderBlue').value;
     let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
     document.body.style.backgroundColor = color;
-    document.querySelector('colorOutput').innerHTML = ': ' + color;
+    document.getElementById('colorOutput').innerHTML = ': ' + color;
 }
 
-document.querySelector('sliderRed').addEventListener('input',changeColor);
-document.querySelector('sliderGreen').addEventListener('input',changeColor);
-document.querySelector('sliderBlue').addEventListener('input',changeColor);
+document.getElementById('sliderRed').addEventListener('input',changeColor);
+document.getElementById('sliderGreen').addEventListener('input',changeColor);
+document.getElementById('sliderBlue').addEventListener('input',changeColor);
