a103ee1bbdaef4af47d2d7e8cc5b628e8433c94e
modda <support@moddapampe.de>
2022-07-02 11:49:16 +0200

    feat: issues repaired

--- clock/script.js
+++ clock/script.js
@@ -1,30 +1,36 @@
-function TimeUpdate() {
-    const time = new Date();
-    const hours = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-
-
-document.querySelector('.digihours').innerText = formatNumberString(hours);
-document.querySelector('.digiminutes').innerText = formatNumberString(minutes);
-document.querySelector('.digiseconds').innerText = formatNumberString(seconds);
-
-const hoursElement = document.querySelector(".analoghours");
-hoursElement.style.setProperty("--hours", hours * 30 + "deg");
-
-const minutesElement = document.querySelector(".analogminutes");
-minutesElement.style.setProperty("--minutes", minutes * 6 + "deg");
-
-const secondsElement = document.querySelector(".analogseconds");
-secondsElement.style.setProperty("--seconds", seconds * 6 + "deg");
-
-document.querySelectorAll(".digidot");
-forEach((e) => e.classList.toggle("digiclock dot--hidden"));
-}  
-
- function formatNumberString(n) {
+function updateTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+  
+    // digi clock
+    document.querySelector(".digihours").innerText =
+      formatNumberString(hours);
+    document.querySelector(".digiminutes").innerText =
+      formatNumberString(minutes);
+    document.querySelector(".digiseconds").innerText =
+      formatNumberString(seconds);
+  
+    // classic clock
+    const hoursEl = document.querySelector(".analoghours");
+    hoursEl.style.setProperty("--hours", hours * 30 + "deg");
+  
+    const minutesEl = document.querySelector(".analogminutes");
+    minutesEl.style.setProperty("--minutes", minutes * 6 + "deg");
+  
+    const secondsEl = document.querySelector(".analogseconds");
+    secondsEl.style.setProperty("--seconds", seconds * 6 + "deg");
+  
+    document
+      .querySelectorAll(".digidots")
+      .forEach((e) => e.classList.toggle("digidots--hidden"));
+  }
+  
+  function formatNumberString(n) {
     let s = "0" + n.toString();
     return s.slice(-2);
-}
-TimeUpdate();
-setInterval(TimeUpdate, 1000);
+  }
+  
+  updateTime();
+  setInterval(updateTime, 1000);
