280e2a9df7c323e7dca4e85f8bbe9662f31bcdbc
modda <support@moddapampe.de>
2022-07-08 09:10:59 +0200

    multiple changes

--- pw-visibility-changer/script.js
+++ pw-visibility-changer/script.js
@@ -11,7 +11,6 @@ button.addEventListener('click', () => {
 	}
 });
 
-
 // Fast, Cheap, Good
 
 let checkboxes = document.querySelectorAll('input');
@@ -21,15 +20,14 @@ for (let checkbox of checkboxes) {
 	checkbox.addEventListener('change', () => {
 		if (checkbox.checked) {
 			if (checkedBoxes.length >= 2) {
-				// Remove recently checked item from array + uncheck
+				
 				const lastCheckbox = checkedBoxes.pop();
 				lastCheckbox.checked = false;
 			}
 			
-			// add new item
 			checkedBoxes.push(checkbox);
 		} else {
-			// remove unchecked item from array
+			
 			const index = checkedBoxes.indexOf(checkbox);
 			checkedBoxes.splice(index, 1);
 		}
--- test/test.js
+++
@@ -1,8 +0,0 @@
-let sum = 0;
-
-function multiply(a, b) {
-    const result = a * b;
-    sum = result;
-
-    console.log(result);
-}
