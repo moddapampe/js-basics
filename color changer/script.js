d0e33de1434e6430a12087a02f7799c92a0065c5
modda <support@moddapampe.de>
2022-07-12 07:58:39 +0200

    probs with js, fix it later

--- color changer/script.js
+++ color changer/script.js
@@ -1,3 +1,6 @@
+// fetch colors from external API and assign the fetched randomColor to the sliders
+
+
 function changeColor(){
     let red = document.getElementById('sliderRed').value;
     let green = document.getElementById('sliderGreen').value;
@@ -10,3 +13,34 @@ function changeColor(){
 document.getElementById('sliderRed').addEventListener('input',changeColor);
 document.getElementById('sliderGreen').addEventListener('input',changeColor);
 document.getElementById('sliderBlue').addEventListener('input',changeColor);
+
+let randomButton = document.getElementById('payme');
+
+randomButton.addEventListener("click", () => {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    
+  .then((response) => {
+      return response.json();
+    })
+    
+    .then((randomObject) => {
+      document.getElementById('colorOutput').innerHTML =
+        randomObject.red + ", " + randomObject.green + ", " + randomObject.blue;
+
+      let = valueBlue = String(randomObject.red);
+      let = valueGreen = String(randomObject.green);
+      let = valueBlue = String(randomObject.blue);
+
+
+
+      document.getElementById('sliderRed').addEventListener('input',changeColor);
+      document.getElementById('sliderGreen').addEventListener('input',changeColor);
+      document.getElementById('sliderBlue').addEventListener('input',changeColor);
+      
+      let rgb ="rgb(" +valueBlue + "," + valueGreen + "," + valueBlue + ")";
+        
+        
+       
+      document.body.style.backgroundColor = rgb;
+    });
+});
