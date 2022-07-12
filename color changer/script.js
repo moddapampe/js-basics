173fcae16e9b9d7682fa2b477bc3053841ba1d4e
modda <support@moddapampe.de>
2022-07-12 08:04:38 +0200

    undefined error

--- color changer/script.js
+++ color changer/script.js
@@ -5,7 +5,9 @@ function changeColor(){
     let red = document.getElementById('sliderRed').value;
     let green = document.getElementById('sliderGreen').value;
     let blue = document.getElementById('sliderBlue').value;
+
     let color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
     document.body.style.backgroundColor = color;
     document.getElementById('colorOutput').innerHTML = ': ' + color;
 }
