c6f21c7f54ede331979b9927c0cbb51dd65ad121
modda <support@moddapampe.de>
2022-07-13 08:34:22 +0200

    modified color changer

--- color changer/script.js
+++ color changer/script.js
@@ -1,6 +1,4 @@
 // fetch colors from external API and assign the fetched randomColor to the sliders
-
-
 function changeColor() {
     let red = document.querySelector('#sliderRed').valueRed;
     let green = document.querySelector('#sliderGreen').valueGreen;
@@ -11,13 +9,10 @@ function changeColor() {
     document.body.style.backgroundColor = color;
     document.querySelector('#colorOutput').innerHTML = ' ' + color;
 }
-
 document.querySelector('#sliderRed').addEventListener('input', changeColor);
 document.querySelector('#sliderGreen').addEventListener('input', changeColor);
 document.querySelector('#sliderBlue').addEventListener('input', changeColor);
-
 let randomButton = document.querySelector('#payme');
-
 randomButton.addEventListener('click', () => {
     fetch("https://dummy-apis.netlify.app/api/color")
 
